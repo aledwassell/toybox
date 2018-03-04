60f8c86e6721f9ff76dcd4091862a6165648f8aa
aledwassell <aledwassell@googlemail.com>
2018-03-04 17:07:39 +0000

    adding binary tree

--- js/angApp.js
+++ js/angApp.js
@@ -1,17 +1,29 @@
 (function () {
     'use strict';
-    var toybox_app = angular.module('toybox_app', [])
-        .factory('grabPhotos', function () {
-            return 'hello'
+    var toybox_app = angular.module('toybox_app', ['ngResource'])
+        .factory('getData', function ($resource) {
+            return $resource('https://jsonplaceholder.typicode.com/posts', {}, {
+                get: {
+                    method: 'GET',
+                    url: 'https://jsonplaceholder.typicode.com/posts/:id',
+                    isArray: false,
+                    id: '1'
+                }
+            })
         })
-        .service('getDataService', ['$http', 'grabPhotos', function ($http, grabPhotos) {
-            var gotData = grabPhotos
+        .service('getDataService', ['$http', 'getData', function ($http, getData) {
+            var gotData = getData
             this.ding = function(){
-                return gotData;
+                console.log(gotData)
+                return gotData.get()
             }
         }])
     .controller('testController', ['$scope', 'getDataService', function ($scope, getDataService) {
         var getDataService = getDataService;
-        $scope.test = getDataService.ding();
+        $scope.gotDataFromServe = getDataService.ding();
+        $scope.test = $scope.gotDataFromServe;
+
+        console.log($scope.test)
+
     }])
 })();
