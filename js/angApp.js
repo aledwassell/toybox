689a6f0d09b364071acbe4d14c2175fac13f2720
aledwassell <aledwassell@googlemail.com>
2018-03-04 15:46:16 +0000

    adding binary tree

--- js/angApp.js
+++ js/angApp.js
@@ -1,8 +1,17 @@
 (function () {
     'use strict';
-    var toybox_app = angular.module('toybox_app', ['ui.bootstrap']);
-    toybox_app.controller('testController', ['$scope', function ($scope) {
-        $scope.test = 'a test'
-        console.log($scope);
+    var toybox_app = angular.module('toybox_app', [])
+        .factory('grabPhotos', function () {
+            return 'hello'
+        })
+        .service('getDataService', ['$http', 'grabPhotos', function ($http, grabPhotos) {
+            var gotData = grabPhotos
+            this.ding = function(){
+                return gotData;
+            }
+        }])
+    .controller('testController', ['$scope', 'getDataService', function ($scope, getDataService) {
+        var getDataService = getDataService;
+        $scope.test = getDataService.ding();
     }])
 })();
