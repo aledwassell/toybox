6793604861570845e152d631659fec103005ed84
Aled's Pro! <pc-user@pc-user-no-MBP.home>
2018-03-04 11:07:39 +0000

    adding binary tree

---
+++ js/recursion.js
@@ -0,0 +1,33 @@
+let countDown = (num) => {
+    if(num === 0) return;
+    countDown(num - 1)
+    console.log(num);
+}
+
+countDown(200);
+
+let categories = [
+    {id: 'animals', 'parent': null},
+    {id: 'mammals', 'parent': 'animals'},
+    {id: 'dogs', 'parent': 'mammals'},
+    {id: 'cats', 'parent': 'mammals'},
+    {id: 'chihuahua', 'parent': 'dogs'},
+    {id: 'labrador', 'parent': 'dogs'},
+    {id: 'persian', 'parent': 'cats'},
+    {id: 'blue', 'parent': 'cats'},
+];
+
+let makeTree = (categories, parent) => {
+    let node = {}
+    categories
+    .filter(c => c.parent === parent)
+    .forEach(c => node[c.id] =
+        makeTree(categories, c.id))
+    return node;
+}
+
+console.log(
+    JSON.stringify(
+        makeTree(categories, null), null, 2
+    )
+);
