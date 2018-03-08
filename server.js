e56623e783d23524ca1d75c7304e9394b51e5682
aledwassell <aledwassell@googlemail.com>
2018-03-08 07:51:31 +0000

    adding binary tree

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = process.env.PORT || 8888;
 
-app.use(express.static('.'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, function () {
     console.log('app running on port: ' + port)
