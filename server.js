94d40e4e917c34b6e7788bde5808b331c034c334
aledwassell <aledwassell@googlemail.com>
2018-03-11 10:06:35 +0000

    adding binary tree

--- server.js
+++ server.js
@@ -3,56 +3,16 @@ let fs = require('fs');
 let path = require('path');
 let port = process.env.PORT = 3000;
 
-http.createServer(function (req, res) {
-    console.log('req ', req.url);
-    console.log('req ', res);
+const hostname = '127.0.0.1';
 
-    let filePath = '.' + req.url;
-    if (filePath == './') {
-        filePath = './public/index.html';
-    }
+const server = http.createServer();
 
-
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let contentType = 'text/html';
-    let mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.svg': 'application/image/svg+xml'
-    };
-
-    contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    res.writeHead(200, { 'Content-Type': contentType });
-                    res.end(content, 'utf-8');
-                });
-            }
-            else {
-                res.writeHead(500);
-                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                res.end();
-            }
-        }
-        else {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf-8');
-        }
-    });
-
-}).listen(port);
-console.log(`Server running at ${port}`);
+server.on('request', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    console.log(req.url);
+})
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
