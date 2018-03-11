acfede40ab2d5ca188e84d2f8be9b22aed8b2c05
aledwassell <aledwassell@googlemail.com>
2018-03-11 18:00:54 +0000

    learning how to properly build a node app, need to get on node js

--- server.js
+++ server.js
@@ -4,35 +4,54 @@ let path = require('path');
 let port = process.env.PORT = 3000;
 
 
-const hostname = '127.0.0.1';
-
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    const {headers, method, url} = req;
-    let body = [];
-    console.log(req.url);
-    console.log(req.method);
-    console.log(req.headers);
-    req.on('data', (chunk) => {
-        body.push(chunk)
-    }).on('end', () => {
-        console.log('body', body)
-        body = Buffer.concat(body).toString();
-    }).on('error', (e) => {
-        console.log(`There was an error ${e}`);
+    req.on('error', (e) => {
+        console.log(e);
+        res.statusCode = 400;
+        res.end();
     });
-
-    res.statusCode = 200;
-
-    // res.setHeader('Content-Type', 'application/json');
-    res.writeHead(200, {'Content-Type': 'application/json'});
-
-    const responseBody = {headers, method, url, body};
-    // res.write(JSON.stringify(responseBody));
-    // res.end();
-    res.end(JSON.stringify(responseBody))
+    res.on('error', (e) => {
+        console.log(e)
+    })
+    if(req.method === 'POST' && req.url === '/echo'){
+        let body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            res.end(body);
+        });
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 })
+
+// server.on('request', (req, res) => {
+//     const {headers, method, url} = req;
+//     let body = [];
+//     req.on('data', (chunk) => {
+//         body.push(chunk)
+//     }).on('end', () => {
+//         body = Buffer.concat(body).toString();
+//         res.end(body)
+//     }).on('error', (e) => {
+//         console.log(`There was an error ${e}`);
+//     });
+//
+//     res.statusCode = 200;
+//
+//     // res.setHeader('Content-Type', 'application/json');
+//     // res.writeHead(200, {'Content-Type': 'application/json'});
+//
+//     const responseBody = {headers, method, url, body};
+//     // res.write(JSON.stringify(responseBody));
+//     // res.end();
+//     // res.end(JSON.stringify(responseBody));
+//
+// })
 
 server.listen(port, () => {
     console.log(`Server running at: ${port}/`);
