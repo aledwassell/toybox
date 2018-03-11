2e715797bdc8d76de15536678cac72faacc3c5e0
aledwassell <aledwassell@googlemail.com>
2018-03-11 08:22:33 +0000

    adding binary tree

--- server.js
+++ server.js
@@ -1,10 +1,57 @@
-const express = require('express');
-var app = express();
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
+let port = process.env.PORT = 3000;
 
-var port = process.env.PORT || 8888;
+http.createServer(function (req, res) {
+    console.log('req ', req.url);
+    console.log('req ', res);
 
-app.use(express.static(__dirname + '/public'));
+    let filePath = '.' + req.url;
+    if (filePath == './') {
+        filePath = './public/index.html';
+    }
 
-app.listen(port, function () {
-    console.log('app running on port: ' + port)
-});
+    let extname = String(path.extname(filePath)).toLowerCase();
+    let contentType = 'text/html';
+    let mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                res.end();
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+
+}).listen(port);
+console.log(`Server running at ${port}`);
