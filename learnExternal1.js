2fd51f0ee0f5f2aa67f6d6c061b86005331487b4
aledwassell <aledwassell@googlemail.com>
2018-03-14 21:26:42 +0000

    finally got the bastard!!!!

--- learnExternal1.js
+++ learnExternal1.js
@@ -1,19 +1,14 @@
 module.exports = function (directory, ext, callback) {
     const fs = require('fs');
     const path = require('path');
-    let files = [];
     fs.readdir(directory, (err, data) => {
         if(err){
             return callback(err, null);
         } else {
-            data.forEach((f) => {
-                if(path.extname(f) === `.${ext}`){
-                    files.push(f);
-                } else {
-                    return;
-                }
+            data = data.filter((f) => {
+                return path.extname(f) === `.${ext}`
             });
-            callback(null, files);
+            callback(null, data);
         }
     });
 
