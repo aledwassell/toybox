8f978311f6073168da0d06e949aac851537eadc6
aledwassell <aledwassell@googlemail.com>
2018-03-14 22:14:35 +0000

    finally got the bastard!!!!

--- server.js
+++ server.js
@@ -5,16 +5,13 @@ let port = process.env.PORT = 3000;
 var request = require("request")
 let content = fs.readFileSync('public/assets/data.json');
 
-let router = require('./api');
+const mongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
-const server = http.createServer();
 
-server.on('request', (req, res) => {
-    req.setRequestHeader({'Content-Type' : 'text/html'})
-        .
-    console.log(req)
-});
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at: ${port}/`);
 });
