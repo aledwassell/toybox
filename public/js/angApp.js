54901b4dd4ae1e379e33a4eb757292429f3e90d9
aledwassell <aledwassell@googlemail.com>
2018-03-16 10:43:48 +0000

    kind of understanding it, so I've injected the setInterval function inside the arguments of another function

--- public/js/angApp.js
+++ public/js/angApp.js
@@ -1,6 +1,13 @@
 (function () {
     'use strict';
-    var toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js'])
+    let canvas = document.createElement('canvas');
+    canvas.width = 300;
+    canvas.height = 300;
+    let ctx = canvas.getContext("2d");
+    let gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(0, 'green');
+    gradient.addColorStop(1, 'red');
+    let toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js'])
         .factory('getData', function ($resource) {
             return $resource('https://jsonplaceholder.typicode.com/posts/1', {}, {
                 get: {
@@ -56,42 +63,74 @@
                 console.log($scope.slider.value);
                 $scope.data[2] = $scope.slider.value;
             });
-            $scope.fill = '';
-            $scope.$on('chart-create', (e, chart) => {
-                console.log(e, chart)
-                var ctx = chart.chart.chart.ctx;
-                var fillPattern = ctx.createLinearGradient(0,0,200,0);
-                fillPattern.addColorStop(0, 'rgba(0,255,0, 1)');
-                fillPattern.addColorStop(1, 'rgba(255,0,0, 1)');
-                ctx.fillStyle = fillPattern;
 
-                $scope.fill = fillPattern;
 
-                console.log(fillPattern)
-            });
+
+
+
+            //$scope.fill = '';
+            //let fill;gradient$scope.$on('chart-create', (e, chart) => {
+            //     console.log('chart', chart);
+            //     var ctx = chart.chart.ctx;
+            //     var fillPattern = ctx.createLinearGradient(0,0,200,0);
+            //     fillPattern.addColorStop(0, 'rgba(0,255,0, 1)');
+            //     fillPattern.addColorStop(1, 'rgba(255,0,0, 1)');
+            //     ctx.fillStyle = fillPattern;
+            //     console.log('chart', chart);
+            //     console.log('ctx', ctx)
+            //     fill = fillPattern;
+            //     // $scope.data.datasets.backgroundColor = fillPattern;
+            //     console.log(fillPattern)
+            // });
 
 
             $scope.labels = ["Download Sales", "In-Store Sales", "gradient"];
             $scope.data = [20, 40, 70, 10];
+
             $scope.colors = [
                 {
-                    backgroundColor: $scope.fill,
-                    pointBackgroundColor: $scope.fill
+                    fillColor: gradient,
+                    strokeColor: gradient,
+                    pointColor: gradient,
+                    pointStrokeColor: gradient,
+                    pointHighlightFill: gradient,
+                    pointHighlightStroke: gradient,
+                    pointBackgroundColor: gradient
                 },
                 {
-                    backgroundColor: 'rgba(255,255,0, 1)',
-                    pointBackgroundColor: 'rgba(255,255,0, 1)'
+                    fillColor: gradient,
+                    strokeColor: gradient,
+                    pointColor: gradient,
+                    pointStrokeColor: gradient,
+                    pointHighlightFill: gradient,
+                    pointHighlightStroke: gradient,
+                    pointBackgroundColor: gradient
                 },
                 {
-                    backgroundColor: 'rgba(255,0,255, 1)',
-                    pointBackgroundColor: 'rgba(255,0,255, 1)'
+                    fillColor: gradient,
+                    strokeColor: gradient,
+                    pointColor: gradient,
+                    pointStrokeColor: gradient,
+                    pointHighlightFill: gradient,
+                    pointHighlightStroke: gradient,
+                    pointBackgroundColor: gradient
                 },
+                {
+                    fillColor: gradient,
+                    strokeColor: gradient,
+                    pointColor: gradient,
+                    pointStrokeColor: gradient,
+                    pointHighlightFill: gradient,
+                    pointHighlightStroke: gradient,
+                    pointBackgroundColor: gradient
+                }
             ];
 
             $scope.options = {
                 cutoutPercentage: 50,
                 rotation: -1 * Math.PI,
-                circumference: Math.PI
+                circumference: Math.PI,
+                animation: false
             };
 
             $scope.slider = {
