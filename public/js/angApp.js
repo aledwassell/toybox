e00411e9de1ab7a48ff1e8b8327e1047a83a124f
aledwassell <aledwassell@googlemail.com>
2018-03-16 11:44:04 +0000

    kind of understanding it, so I've injected the setInterval function inside the arguments of another function

--- public/js/angApp.js
+++ public/js/angApp.js
@@ -1,13 +1,6 @@
 (function () {
     'use strict';
-    let canvas = document.createElement('canvas');
-    canvas.width = 300;
-    canvas.height = 300;
-    let ctx = canvas.getContext("2d");
-    let gradient = ctx.createLinearGradient(0, 0, 0, 300);
-    gradient.addColorStop(0, 'green');
-    gradient.addColorStop(1, 'red');
-    let toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js'])
+    var toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js'])
         .factory('getData', function ($resource) {
             return $resource('https://jsonplaceholder.typicode.com/posts/1', {}, {
                 get: {
@@ -59,17 +52,39 @@
 
 
         .controller("chartsCtrl", ['$scope', function ($scope) {
+            let randN = getRandomInt(20, 100);
+            $scope.data = [randN, 40, 0, 0];
+            function getRandomInt(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+            }
+
+            setInterval(function () {
+                randN = getRandomInt(20, 100);
+            }, 500);
+
+            $scope.data = [randN, 40, 0, 0];
+
+            function changeDat (sliderVal) {
+                $scope.data[3] = sliderVal;
+            }
             $scope.$watch('slider.value', function () {
                 console.log($scope.slider.value);
-                $scope.data[2] = $scope.slider.value;
+                // $scope.data[3] = $scope.slider.value;
+                changeDat($scope.slider.value)
             });
 
-
-
-
-
-            //$scope.fill = '';
-            //let fill;gradient$scope.$on('chart-create', (e, chart) => {
+            // let canvas = document.createElement('canvas');
+            // canvas.width = 300;
+            // canvas.height = 300;
+            // let ctx01 = canvas.getContext("2d");
+            // let gradient = ctx01.createLinearGradient(0, 0, 0, 300);
+            // gradient.addColorStop(0, 'green');
+            // gradient.addColorStop(1, 'red');
+            //
+            // let fill = '';
+            // $scope.$on('chart-create', (e, chart) => {
             //     console.log('chart', chart);
             //     var ctx = chart.chart.ctx;
             //     var fillPattern = ctx.createLinearGradient(0,0,200,0);
@@ -84,46 +99,47 @@
             // });
 
 
-            $scope.labels = ["Download Sales", "In-Store Sales", "gradient"];
-            $scope.data = [20, 40, 70, 10];
+            $scope.labels = ["Download Sales", "In-Store Sales", "more data", "and some more data"];
+            // $scope.data = [20, 40, 0, 0];
 
             $scope.colors = [
                 {
-                    fillColor: gradient,
-                    strokeColor: gradient,
-                    pointColor: gradient,
-                    pointStrokeColor: gradient,
-                    pointHighlightFill: gradient,
-                    pointHighlightStroke: gradient,
-                    pointBackgroundColor: gradient
+                    fillColor: '#00ffff',
+                    strokeColor: '#00ffff',
+                    pointColor: '#00ffff',
+                    pointStrokeColor: '#00ffff',
+                    pointHighlightFill: '#00ffff',
+                    pointHighlightStroke: '#00ffff',
+                    pointBackgroundColor: '#00ffff'
                 },
                 {
-                    fillColor: gradient,
-                    strokeColor: gradient,
-                    pointColor: gradient,
-                    pointStrokeColor: gradient,
-                    pointHighlightFill: gradient,
-                    pointHighlightStroke: gradient,
-                    pointBackgroundColor: gradient
+                    fillColor: '#00ff00',
+                    strokeColor: '#00ff00',
+                    pointColor: '#00ff00',
+                    pointStrokeColor: '#00ff00',
+                    pointHighlightFill: '#00ff00',
+                    pointHighlightStroke: '#00ff00',
+                    pointBackgroundColor: '#00ff00'
                 },
                 {
-                    fillColor: gradient,
-                    strokeColor: gradient,
-                    pointColor: gradient,
-                    pointStrokeColor: gradient,
-                    pointHighlightFill: gradient,
-                    pointHighlightStroke: gradient,
-                    pointBackgroundColor: gradient
+                    fillColor: '#ffff00',
+                    strokeColor: '#ffff00',
+                    pointColor: '#ffff00',
+                    pointStrokeColor: '#ffff00',
+                    pointHighlightFill: '#ffff00',
+                    pointHighlightStroke: '#ffff00',
+                    pointBackgroundColor: '#ffff00'
                 },
                 {
-                    fillColor: gradient,
-                    strokeColor: gradient,
-                    pointColor: gradient,
-                    pointStrokeColor: gradient,
-                    pointHighlightFill: gradient,
-                    pointHighlightStroke: gradient,
-                    pointBackgroundColor: gradient
-                }
+                    fillColor: '#ff0000',
+                    strokeColor: '#ff0000',
+                    pointColor: '#ff0000',
+                    pointStrokeColor: '#ff0000',
+                    pointHighlightFill: '#ff0000',
+                    pointHighlightStroke: '#ff0000',
+                    pointBackgroundColor: '#ff0000'
+                },
+
             ];
 
             $scope.options = {
@@ -134,7 +150,7 @@
             };
 
             $scope.slider = {
-                value: 20,
+                value: 0,
                 options: {
                     disabled: false,
                     floor: 0,
