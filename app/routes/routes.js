8e2cfa7579dca373458ec45ab6752f69fc1b62ab
aledwassell <aledwassell@googlemail.com>
2018-03-16 07:22:05 +0000

    Good, its working on mongoDB version 2.2.33

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,20 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = (app, db) => {
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id
+        const details = {'_id' : new ObjectID(id)};
+        db.collection('notes').findOne(details, (e, item) => {
+            if(e){
+                res.send({'error':'you got an error'})
+            } else {
+                res.send(item)
+            }
+        })
+    })
+
     app.post('/notes', (req, res) => {
-        const note = {text: req.body.body, title: req.body.title};
+        const note = {title: req.body.title, text: req.body.body};
         db.collection('notes').insert(note, (e, result) => {
             if(e) {
                 res.send({'error' : 'error was found'})
