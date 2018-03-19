d236000f67f84e8b257bfe318ba3a262927c6758
aledwassell <aledwassell@googlemail.com>
2018-03-19 21:01:52 +0000

    Adding Quagga barcode scanner, not working right now

--- public/js/quaggaConfig.js
+++ public/js/quaggaConfig.js
@@ -1,28 +1,39 @@
-if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-    // safely access `navigator.mediaDevices.getUserMedia`
-    console.log('browser be cool')
-    Quagga.init({
-        inputStream : {
-            name : "Live",
-            type : "LiveStream",
-            constraints: {
-                width: {min: 640},
-                height: {min: 480},
-                facingMode: "environment",
-                aspectRatio: {min: 1, max: 2}
-            },rs
-            target: document.querySelector('#quaggaTarget')    // Or '#yourElement' (optional)
-        },
-        decoder : {
-            readers : ["code_128_reader"]
+(function(){
+    'use strict';
+    if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+        // safely access `navigator.mediaDevices.getUserMedia`
+        console.log('browser be cool')
+        let app = {
+
+            init: function () {
+                Quagga.init(this.state, function (err) {
+                    if (err) {
+                        console.log('You got an error: ', err);
+                        return;
+                    }
+                    console.log("Initialization finished. Ready to start");
+                    Quagga.start();
+                })
+            },
+            state: {
+                inputStream: {
+                    name: "Live",
+                    type: "LiveStream",
+                    constraints: {
+                        width: {min: 640},
+                        height: {min: 480},
+                        facingMode: "environment",
+                        aspectRatio: {min: 1, max: 2}
+                    },
+                    target: document.querySelector('#quaggaTarget')    // Or '#yourElement' (optional)
+                },
+                decoder: {
+                    readers: ["code_128_reader"]
+                }
+            }
         }
-    }, function(err) {
-        if (err) {
-            console.log('You got an error: ', err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-    })
-}
+        app.init()
+    }
+}());
+
 
