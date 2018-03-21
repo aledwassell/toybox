3397d699731f2f5b4da009d5a23e64c5057823af
aledwassell <aledwassell@googlemail.com>
2018-03-21 19:49:56 +0000

    quagga barcode scanner working

--- public/js/angApp.js
+++ public/js/angApp.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    var toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js'])
+    var toybox_app = angular.module('toybox_app', ['ngResource', 'rzModule', 'chart.js', 'angularjs-gauge'])
         .factory('data', function ($resource) {
             return $resource('/notes', {}, {
                 post: {
@@ -38,8 +38,44 @@
         }])
 
         .controller('quaggaController', ['$scope', function($scope){
+            // if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
+            //     // safely access `navigator.mediaDevices.getUserMedia`
+            //     console.log('browser be cool')
+            //     let app = {
+            //
+            //         init: function () {
+            //             Quagga.init(this.state, function (err) {
+            //                 if (err) {
+            //                     console.log('You got an error: ', err);
+            //                     return;
+            //                 }
+            //                 console.log("Initialization finished. Ready to start");
+            //                 Quagga.start();
+            //             })
+            //         },
+            //         state: {
+            //             inputStream: {
+            //                 name: "Live",
+            //                 type: "LiveStream",
+            //                 constraints: {
+            //                     width: {min: 640},
+            //                     height: {min: 480},
+            //                     facingMode: "environment",
+            //                     aspectRatio: {min: 1, max: 2}
+            //                 },
+            //                 target: document.querySelector('#quaggaTarget')    // Or '#yourElement' (optional)
+            //             },
+            //             decoder: {
+            //                 readers: ["code_128_reader"]
+            //             }
+            //         }
+            //     }
+            //     app.init()
+            // }
 
-
+        }])
+        .controller('gaugeController', ['$scope', function ($scope) {
+            
         }])
 
 
