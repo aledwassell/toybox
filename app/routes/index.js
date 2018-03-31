628ddf7e894a62c9a5aa400d131f51e870d733a9
aledwassell <aledwassell@googlemail.com>
2018-03-31 18:04:54 +0100

    setting up weather api

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 const route = require('./routes');
 const products = require('./products');
-const scanner = require('./scanner')
+const scanner = require('./scanner');
+const weather = require('./weather')
 module.exports = ((app, db) => {
     route(app, db);
     products(app, db);
     scanner(app, db);
+    weather(app);
 });
--- app/routes/weather.js
+++ app/routes/weather.js
@@ -19,11 +19,13 @@ module.exports = (app) => {
             res.status(404).send(`You need to provide a location, ${req.params.place} is not valid.`);
             return false;
         }
-        const location = req.params.location;
-        request.get({
-            url: `api url&${req.params.place}`
+        const apiKey = '6bd81b03a64f7ae2cb9240d3271279aa';
+        const location = req.params.place;
+        console.log(location)
+        app.get({
+            url: `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`
         }, (e, response, body) => {
-            if(!e && response.status.code === 200) {
+            if(!e && response.status === 200) {
                 res.send(body)
             } else if (e){
                 res.status(404).send(`There was an error in your request ${e}`);
