40484f0bb092a0373f063639735baf47a51c026d
aledwassell <aledwassell@googlemail.com>
2018-03-31 11:35:24 +0100

    setting up query api in node app

--- server.js
+++ server.js
@@ -11,6 +11,45 @@ const app = express();
 const urlencodedParser = bodyParser.urlencoded({extended: true})
 const jsonParser = bodyParser.json();
 
+const dat = [
+    {id: 1, name: 'Clive'},
+    {id: 2, name: 'Dave'},
+    {id: 3, name: 'Peter'},
+    {id: 4, name: 'Pat'},
+    {id: 5, name: 'Carl'},
+];
+
+const getElementById = (id, array) => {
+    let returnedVal;
+    for(let i = 0; i < array.length; i++) {
+        if(i.id === id){
+            console.log(i)
+            returnedVal = i;
+        } else {
+            returnedVal = undefined;
+        }
+    }
+
+
+
+    console.log(returnedVal)
+    if(returnedVal){
+        return returnedVal
+    } else {
+        return undefined
+    }
+}
+
+app.get('/api/:id', (req, res) => {
+    const gotDat = getElementById(req.params.id, dat);
+    if(gotDat !== undefined){
+        res.send(gotDat);
+    } else {
+        res.status(404).send(`an error occured.`)
+    }
+
+})
+
 
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
