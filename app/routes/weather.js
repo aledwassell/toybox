77af4f48df6788bbbdb0d79650ebae788e14acad
aledwassell <aledwassell@googlemail.com>
2018-04-01 10:47:24 +0100

    setting up weather api

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -21,12 +21,13 @@ module.exports = (app) => {
         }
         const apiKey = '6bd81b03a64f7ae2cb9240d3271279aa';
         const location = req.params.place;
-        console.log(location)
-        app.get({
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`
-        }, (e, response, body) => {
-            if(!e && response.status === 200) {
+        console.log(location);
+
+        request.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}` , (e, res, body) => {
+            console.log(e, res, body)
+            if(!e && res.status === 200) {
                 res.send(body)
+                console.log(body)
             } else if (e){
                 res.status(404).send(`There was an error in your request ${e}`);
             }
